8df8a4b7a5e0571e2d2b37a4eed66231d785325e
Psarmidani <Psarmidani@MacBook-Air-van-Puya.fritz.box>
2015-09-07 12:00:49 +0200

    Example WhoIs script.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# neostrada-whois
-Simpel en snel je eigen whois opzetten
+![Neostrada](https://www.neostrada.nl/asset/nx/images/logo.png)
+=================
+
+# neostrada-whois PHP voorbeeldscript
+Simpel en snel je eigen whoIs opzetten
+
+## API Key en Secret ##
+Om de script te kunnen gebruiken heb je een API Key en Secret nodig.
+Log in op https://www.neostrada.nl/inloggen.html en klik vervolgens links op API in het menu
+
+## Aanpassing ##
+Pas de volgende code aan in example.php.
+
+```js
+/**
+ * Your API information can be found after logging in to the website
+ */
+
+$API->SetAPIKey('[API_Key]');
+$API->SetAPISecret('[API_Secret]');
+```
+
+## License ##
+[BSD (Berkeley Software Distribution) License](http://www.opensource.org/licenses/bsd-license.php).
+Copyright (c) 2012, Avot Media BV
+
+## Support ##
+[www.neostrada.nl](https://www.neostrada.nl) - support@neostrada.nl 
---
+++ function/functions.php
@@ -0,0 +1,160 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: Puya
+     * Date: 10-7-2015
+     * Time: 08:48
+     */
+
+    require_once dirname(__FILE__) . '/neostrada.inc.php';
+
+    class domainAPI extends Neostrada
+    {
+
+        /**
+         * @param $API
+         * @param $domain
+         * @param $inputExt
+         * @return mixed
+         * Return information about the domain and extension.
+         */
+        public function getDomain($API,$domain, $inputExt)
+        {
+
+            $API->prepare('whois', array(
+                'domain' => $domain,
+                'extension' => $inputExt
+            ));
+            $API->execute();
+            $ResultDomain = $API->fetch();
+
+            echo "<script type='text/javascript'>
+                                    if(document.getElementById('whoistld'))
+                                    {
+                                        document.getElementById('whoistld').innerHTML=\"".$domain.".".$inputExt."\";
+                                    }
+                                  </script>";
+
+            $data = $ResultDomain;
+
+            return $data;
+        }
+
+        /**
+         * @param $CountResult
+         * @param $Result
+         * @return string
+         * Count the results for the form dropdown
+         */
+        public function GetExtensions($CountResult, $Result){
+
+            $Options = '';
+            for ($i = 0; $i < $CountResult; $i++)
+            {
+
+                $Extension = $Result['extensions'][$i];
+
+                $inputExt = $this->getouterSubstring($Extension, ';', 0);
+
+
+                $Options .= '<option name = "ext" value="' . $inputExt . '">.' . $inputExt . '</option>' . "\n";
+            }
+            return $Options;
+
+        }
+
+        /**
+         * @param $Post
+         * @return bool
+         * Validate domain input POST
+         */
+        public function CheckDomain($Post){
+
+            $void = TRUE;
+            if (empty($Post))
+            {
+                echo 'Invalid domain';
+                $void = FALSE;
+            } elseif (strlen($Post) < 2)
+            {
+                echo 'Domain is too short';
+                $void = FALSE;
+            } elseif (strlen($Post) > 60)
+            {
+                echo 'Domain is too long';
+                $void = FALSE;
+            } elseif (!preg_match('/^([a-zA-Z0-9_-]+)$/i', $Post))
+            {
+                echo 'Special characters not allowed';
+                $void = FALSE;
+            }
+                return $void;
+        }
+
+        /**
+         * @param $API
+         * @param $Domain
+         * @param $Result
+         * @param bool $i
+         * Explode the array and get the extensions
+         */
+
+        public function GetInfo($API, $Domain, $Result, $i = FALSE)
+        {
+
+            $Extension = $Result[$i];
+
+            $inputExt = $this->getouterSubstring($Extension, ';', 0);
+
+            $Check = $this->getDomain($API,$Domain,$inputExt);
+
+
+            $this->GetResults($Domain, $inputExt, $i, $Check);
+
+            flush();
+        }
+
+        /**
+         * @param $string
+         * @param $delim
+         * @param $block
+         * @return string
+         * Strip the extension array
+         */
+
+        public function getouterSubstring($string, $delim, $block)
+        {
+            // "foo a foo" becomes: array(""," a ","")
+            $string = explode($delim, $string, 2);
+            return isset($string[$block]) ? $string[$block] : '';
+        }
+
+        /**
+         * @param $domein
+         * @param $extensie
+         * @param $i
+         * @param $Check
+         * Print the result
+         */
+        function GetResults($domein, $extensie, $i, $Check)
+        {
+
+            $k = ((($i + 1) % 2) == 0) ? 1 : 2;
+
+            if($Check['code'] == 210){
+
+                $getResult = '<td class="whoisrow'.$k.'"><img src="img/check.png" width="16" height="16" alt="check"></td>';
+            }elseif($Check['code'] == 211){
+
+                $getResult = '<td class="whoisrow'.$k.'"><img src="img/cross.png" width="16" height="16" alt="cross"></td>';
+            }
+
+            echo '<tr><td class="whoisrow'.$k.'">
+                <a href="http://www.'.$domein.'.'.$extensie.'" target="_blank">'.$domein.'.'.$extensie.'</a>
+              </td>'.$getResult;
+            echo  '
+              <td class="whoisrow'.$k.'">'.$Check['description']; //begin de laatste kolom
+
+            echo '</td></tr>';
+        }
+    }
